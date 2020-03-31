5ca383fa2ca9936b7f7647e4ea4299a9a3be5dd4
chrisjamesr <chrisjames.r@gmail.com>
2020-03-31 17:00:15 -0400

    add mongodb connection string

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,10 @@
+const connectionString = `mongodb+srv://chrisjames:TScvwZDqg7MlCGXe@cluster0-twxcl.mongodb.net/test?retryWrites=true&w=majority`
+
 module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4s',
+  mongoURI: connectionString, 
   cookieKey: '123123123'
 };
+
