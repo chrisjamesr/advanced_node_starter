154bb10c191e049cd4c78e4ef8424affb2c9f694
chrisjamesr <chrisjames.r@gmail.com>
2020-04-03 08:44:46 -0400

    remove console.log statements . Create new object with query string and collection name to act as unique key for redis cache

--- services/cache.js
+++ services/cache.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function(){
-    console.log('im about to run a query');
+
+    const key = Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    });
+
+    console.log(key);
     return exec.apply(this, arguments);
 }
