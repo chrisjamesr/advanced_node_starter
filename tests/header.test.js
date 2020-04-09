47dce9d3f4fc379ae7bedc03313ba7e6e94b2206
chrisjamesr <chrisjames.r@gmail.com>
2020-04-08 22:26:37 -0400

    add additional test for beginning of google oauth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,35 @@
-test('Adds two numbers', () => {
-    const sum = 1 + 2;
-    expect(sum).toEqual(3);
+const puppeteer = require('puppeteer'); 
+let page, browser;
+
+beforeEach( async ()=> {
+    browser = await puppeteer.launch({
+        headless: false, 
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+},10000);
+
+afterEach( async () => {
+    await browser.close() 
 });
+
+test('the header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    
+    expect(text).toEqual('Blogster');
+});   
+
+test('clicking login link kicks off OAuth flow', async () => {
+    await page.click('.right a');
+    const url = await page.url()
+    
+    expect(url).toMatch(/accounts\.google\.com/);
+});   
+
+test('confirm ', async () => {
+    // await page.click('.right a');
+    // const url = await page.url()
+    
+    // expect(url).toMatch(/accounts\.google\.com/);
+});   
+ 
