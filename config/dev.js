af8f4b62163e8d5c30b63280a5806ad9f883b819
chrisjamesr <chrisjames.r@gmail.com>
2020-04-11 10:38:55 -0400

    remove sensitive info:

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,8 @@
-// const connectionString = `mongodb+srv://chrisjames:TScvwZDqg7MlCGXe@cluster0-twxcl.mongodb.net/test?retryWrites=true&w=majority`
-const connectionString = `mongodb://chris:4l3xk4l1@ds141815.mlab.com:41815/blog_dev`
+require('dotenv').config();
 
 module.exports = {
-    googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-    googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-    mongoURI: connectionString, 
-    cookieKey: '123123123'
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.DB_CONN_STRING, 
+    cookieKey: process.env.COOKIE_KEY
 };
