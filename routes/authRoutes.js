936bd1c1ef608782b813c374107b7eaf8b5c557b
chrisjamesr <chrisjames.r@gmail.com>
2020-04-13 16:53:55 -0400

    add initial and callback routes for github oauth strategy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,6 +16,16 @@ module.exports = app => {
     }
   );
 
+  app.get(
+    '/auth/github',
+   passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/blogs');
+  });
+
   app.get('/auth/logout', (req, res) => {
     req.logout();
     res.redirect('/');
