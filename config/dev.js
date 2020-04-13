7b56cad0aee64e86a968e8ebfd40d35c816c2126
chrisjamesr <chrisjames.r@gmail.com>
2020-04-13 16:52:31 -0400

    add properties for gh client id & secret

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.DB_CONN_STRING, 
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    githubClientID: process.env.GITHUB_CLIENT_ID,
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
 };
