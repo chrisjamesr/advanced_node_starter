9ec87a19433aa2775bb2607b521adcb73050917c
chrisjamesr <chrisjames.r@gmail.com>
2020-04-14 10:10:23 -0400

    create proxy to wrap existing puppeteer page/browser objects with custom build function to launch new page

---
+++ tests/helpers/page.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+
+class Page {
+    static async build(){
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+
+        const page = await browser.newPage();
+        const customPage = new Page(page);
+
+        return new Proxy(customPage, {
+            get: function(target, property){
+                return target[property] || page[property] || browser[property];
+            }
+        });
+    }
+
+    constructor(page){
+        this.page = page;
+    }
+}
+
+module.exports = Page;
