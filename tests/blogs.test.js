675299f0c19ce968a44b7568acd43b3128375573
chrisjamesr <chrisjames.r@gmail.com>
2020-04-15 08:57:12 -0400

    add nested describe statement for invalid inputs to check for correct errors. Add describe statement for correct input and confirm review page

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -13,13 +13,48 @@ afterEach( async () => {
     await page.close() 
 });
 
-test.only('When logged in, can see blog create form', async () => {
-    await page.login();
-    await page.click('a.btn-floating');
-    // const url = await page.url();
-    // expect(url).toMatch(/blogs\/new/);
+describe('When logged in', async () => {
+    beforeEach(async ()=> {
+        await page.login();
+        await page.click('a.btn-floating');
+    });
 
-    const label = await page.getContentsOf('form label');
-    expect(label).toEqual('Blog Title');
+    test('Can see blog create form', async () => {
+        const label = await page.getContentsOf('form label');
+        expect(label).toEqual('Blog Title');
+    });
 
+    describe('And using valid form inputs', async () => {
+        beforeEach(async () => {
+            await page.type('div.title input', 'Blog Test Title');
+            await page.type('div.content input', 'Blog Test Content');
+            await page.click('form button');
+        });
+
+        test('Submitting takes user to review screen', async () => {
+            const headerText = await page.getContentsOf('form h5');
+
+            expect(headerText).toEqual('Please confirm your entries');
+        })
+        
+        test('Submitting and saving adds blog to index page', async () => {
+            await page.click('button.green');
+            
+        });
+    });
+
+    describe('And using invalid inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+
+        test('the form shows an error message', async () => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const contentError = await page.getContentsOf('.content .red-text');
+
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        });
+    });
+    
 })
