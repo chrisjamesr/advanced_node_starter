b2970feacc789e23fe79da83662c5223a45725b8
chrisjamesr <chrisjames.r@gmail.com>
2020-04-14 20:56:50 -0400

    add test for new blog form. remove timeout interval from beforeEach function

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -7,8 +7,19 @@ let page;
 beforeEach( async ()=> {
     page = await Page.build();
     await page.goto('localhost:3000');
-},15000);
+});
 
 afterEach( async () => {
     await page.close() 
 });
+
+test.only('When logged in, can see blog create form', async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+    // const url = await page.url();
+    // expect(url).toMatch(/blogs\/new/);
+
+    const label = await page.getContentsOf('form label');
+    expect(label).toEqual('Blog Title');
+
+})
