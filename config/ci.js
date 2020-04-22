9c57b33b8216e3d3d468ec986796ad0e60fda37d
chrisjamesr <chrisjames.r@gmail.com>
2020-04-21 22:30:32 -0400

    add redisUrl from services/cache.js

---
+++ config/ci.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.DB_CONN_STRING, 
+    cookieKey: process.env.COOKIE_KEY,
+    githubClientID: process.env.GITHUB_CLIENT_ID,
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+    redisUrl = 'redis://127.0.0.1:6379'
+};
