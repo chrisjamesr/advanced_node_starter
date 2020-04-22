342534a1f47cce589e46a83db1ee66d07ecfa5a6
chrisjamesr <chrisjames.r@gmail.com>
2020-04-22 16:43:17 -0400

    add state, onFileChange function and input element to component

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = {file: null};
   renderFields() {
     const { formValues } = this.props;
 
@@ -46,13 +47,23 @@ class BlogFormReview extends Component {
 
     submitBlog(formValues, history);
   }
+  
+  onFileChange(event){
+    console.log(event.target.files);
+    this.setState({file: event.target.files});
+  }
 
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
-
+        <h5>Add An Image</h5>
+        <input 
+          onChange={this.onFileChange.bind(this)}
+          type="file" 
+          accept="image/*" 
+          /><br/>
         {this.renderButtons()}
       </form>
     );
