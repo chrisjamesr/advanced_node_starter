0ef9e1943f38af6d70dd28a77998f710116977f3
chrisjamesr <chrisjames.r@gmail.com>
2020-04-22 07:25:33 -0400

    fix typo on mongoURL

--- config/ci.js
+++ config/ci.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: 'mongodb://127.0.0.127017/blog_ci',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
     cookieKey: process.env.COOKIE_KEY,
     githubClientID: process.env.GITHUB_CLIENT_ID,
     githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
