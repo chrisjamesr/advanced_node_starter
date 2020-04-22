eeea541d1f0d2085e5408b36d779680bad4e9f82
chrisjamesr <chrisjames.r@gmail.com>
2020-04-21 22:32:37 -0400

    add mongo default connection string for travisCI

--- config/ci.js
+++ config/ci.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.DB_CONN_STRING, 
+    mongoURI: 'mongodb://127.0.0.127017/blog_ci',
     cookieKey: process.env.COOKIE_KEY,
     githubClientID: process.env.GITHUB_CLIENT_ID,
     githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
