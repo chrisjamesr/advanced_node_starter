9d3c2dcaf864f95d1c4bd0bdb0a718c1dba2503f
chrisjamesr <chrisjames.r@gmail.com>
2020-04-21 23:21:43 -0400

    fix typo on redis url

--- config/ci.js
+++ config/ci.js
@@ -7,5 +7,5 @@ module.exports = {
     cookieKey: process.env.COOKIE_KEY,
     githubClientID: process.env.GITHUB_CLIENT_ID,
     githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
-    redisUrl = 'redis://127.0.0.1:6379'
+    redisUrl: 'redis://127.0.0.1:6379'
 };
