ff693601e8aad04e10ce824085edd83bb5d81e3f
chrisjamesr <chrisjames.r@gmail.com>
2020-04-24 08:17:53 -0400

    add region property to new AWS.S3 object constructor. Fixed upload problem

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,19 +1,23 @@
 const AWS = require('aws-sdk');
 const uuid = require('uuid/v1');
 const keys = require('../config/keys');
+const requireLogin = require('../middlewares/requireLogin');
 
 const s3 = new AWS.S3({
     accessKeyId: keys.accessKeyId,
-    secretAccessKey: keys.secretAccessKey
+    secretAccessKey: keys.secretAccessKey,
+    region: 'us-east-2'
 });
 
 module.exports = app => {
-    app.get('/api/upload', (req, res) => {
+    app.get('/api/upload', requireLogin, (req, res) => {
         const key = `${req.user.id}/${uuid()}.jpeg`;
+        
         s3.getSignedUrl('putObject', {
             Bucket: 'blog-test-bucket-00',
-            ContentType:'jpeg',
+            ContentType: 'image/jpeg',
             Key: key,
-        })
+        }, (err, url) => res.send({key, url}));
     });
 };
+
